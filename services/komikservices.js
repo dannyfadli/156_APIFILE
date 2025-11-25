3daf74c96cf6720e686d062d64ea28fa75bec015
danny fadli <danyfdly95@gmail.com>
2025-11-25 14:50:19 +0700

    Add function to retrieve Komik by ID

--- services/komikservices.js
+++ services/komikservices.js
@@ -16,5 +16,29 @@ async function createKomik(database, komikData) {
     imageData: imageData || null,
     });
 
-return newKomik;
+    return newKomik;
+}
+async function getAllKomik(database) {
+    const komiks = await database.Komik.findAll();
+
+
+    return komiks.map(k => {
+        if (k.imageData) {
+            k.imageData = k.imageData.toString('base64');
+        }
+        return k;
+    });
+}
+
+async function getKomikById(database, id) {
+    const komik = await database.Komik.findByPk(id);
+    if (!komik) throw new Error('Komik tidak ditemukan');
+
+
+    if (komik.imageData) {
+    komik.imageData = komik.imageData.toString('base64');
+    }
+
+
+    return komik;
 }
