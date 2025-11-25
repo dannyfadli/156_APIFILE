c078740f7d58594fb650f4266445ddfa8c61e0d9
danny fadli <danyfdly95@gmail.com>
2025-11-25 14:24:00 +0700

    mematikan app use biar

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/api', apiRoute);
+// app.use('/api', apiRoute);
 
 async function startServer() {
     await connetDatabase();
