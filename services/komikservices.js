306e781731c848f0097c754bec31440b58b08cc1
danny fadli <danyfdly95@gmail.com>
2025-11-25 14:47:46 +0700

    Create Komik entry in database

--- services/komikservices.js
+++ services/komikservices.js
@@ -1,7 +1,20 @@
 async function createKomik(database, komikData) {
-const { title, description, author, imageType, imageName, imageData } = komikData;
+    const { title, description, author, imageType, imageName, imageData } = komikData;
 
 
-if (!title || !description || !author) {
-throw new Error('Title, description, dan author wajib diisi');
+    if (!title || !description || !author) {
+    throw new Error('Title, description, dan author wajib diisi');
+    }
+
+
+    const newKomik = await database.Komik.create({
+    title,
+    description,
+    author,
+    imageType: imageType || null,
+    imageName: imageName || null,
+    imageData: imageData || null,
+    });
+
+return newKomik;
 }
